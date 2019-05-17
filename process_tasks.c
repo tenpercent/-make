9dfef8ab00274b3fefd27bacfdab0a59eb61f035
Max Podkorytov <max.podkorytoff@yandex.ru>
2019-05-17 00:39:06 -0400

    test whether this approach works

--- process_tasks.c
+++ process_tasks.c
@@ -1,9 +1,11 @@
 #include <pthread.h>
 #include <stdatomic.h>
+#include <stdio.h>
 
 struct tmem {
     _Atomic int *counter;
     int tid;
+    int *arr;
 };
 
 static void* task(void *arg) {
@@ -11,6 +13,7 @@ static void* task(void *arg) {
     int i = *params->counter;
     while (i > 0) {
         i = --(*params->counter);
+	params->arr[i] = 1 + params->tid;
     }
     return NULL;
 }
@@ -19,8 +22,9 @@ int process_tasks(void) {
     const int nthr = 8;
     pthread_t threads[nthr];
     _Atomic int counter = 100000;
+    int arr[100000] = {0};
     for (int i = 0; i < nthr; ++i) {
-	struct tmem arg = {.counter = &counter, .tid = i};    
+	struct tmem arg = {.counter = &counter, .tid = i, .arr = arr};    
         pthread_create(threads + i, 
 		       NULL, /* default attributes */
 		       &task,
@@ -28,6 +32,14 @@ int process_tasks(void) {
     }
     for (int i = 0; i < nthr; ++i) {
         pthread_join(threads[i], NULL);
+    }
+    // check
+    for (int i = 0; i < 100000; ++i) {
+       // printf("%d\n", arr[i]);	    
+       if (arr[i] == 0) {
+	   printf("AAA!");    
+           return -1;
+       }
     }
     return 0;
 }
